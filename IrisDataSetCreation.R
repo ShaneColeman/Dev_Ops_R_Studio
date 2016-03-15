c879d165063fc8d4b65516e46130643da2cb53df
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-03-15 18:49:05 +0000

    RStudioDevOps R file renamed to IrisDataSetCreation R file

---
+++ IrisDataSetCreation.R
@@ -0,0 +1,5 @@
+#Dev Ops utilising R Studio
+
+irisDataSet <- iris
+
+write.csv(irisDataSet, file = "IrisDataSet.csv", row.names = FALSE)
--- RStudioDevOps.R
+++
@@ -1,5 +0,0 @@
-#Dev Ops utilising R Studio
-
-irisDataSet <- iris
-
-write.csv(irisDataSet, file = "IrisDataSet.csv",row.names = FALSE)
